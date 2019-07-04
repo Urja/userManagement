3528bcf7df03759bfea6983b329e79bffc655e33
urja <urja1986@gmail.com>
2019-07-04 16:00:43 +0200

    Swagger configuration

--- src/main/java/application/controller/UserController.java
+++ src/main/java/application/controller/UserController.java
@@ -4,6 +4,8 @@ import application.dto.AuthenticationRequest;
 import application.dto.RegistrationRequest;
 import application.dto.ResetPasswordRequest;
 import application.service.Impl.UserServiceImpl;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +20,15 @@ public class UserController {
     @Autowired
     UserServiceImpl userService;
 
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "User Registered successfully for ${userId}")
+    })
     @PostMapping
     public ResponseEntity<String> registration(@RequestBody RegistrationRequest registrationRequest){
        long userId = userService.registration(registrationRequest);
-        return ResponseEntity.status(HttpStatus.CREATED).body("User Registered sucessfully for" + userId); //TODO: Send url instead
+        return ResponseEntity.status(HttpStatus.CREATED).body("User Registered successfully for" + userId); //TODO: Send url instead
     }
+
 
     @PostMapping("/authenticate")
     public ResponseEntity authenticate(@RequestBody AuthenticationRequest authenticationRequest){
